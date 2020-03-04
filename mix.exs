1a9f0bebe0b79d7de842212a5ac44de2dbfb236c
蒼時弦也 <elct9620@frost.tw>
2020-03-05 02:15:59 +0800

    Update app name

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule MUD.MixProject do
 
   def project do
     [
-      app: :rpg,
+      app: :mud,
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
